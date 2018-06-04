a94d0aab000a5af36a5b2cb00b9a056cb9b31d70
Alexius Calamus <AlexiusCalamus@users.noreply.github.com>
2018-06-05 01:46:09 +0200

    Add Visuals, Randomize gallery item and some CSS change

--- Controllers/InfoDataController.cs
+++ Controllers/InfoDataController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace LIEC_Website.Controllers
 {
     [Route("api/[controller]")]
     public class InfoDataController : Controller
-    {
+    {        
+        private static IHostingEnvironment _hostingEnvironment;
         private static string[] _tags = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -19,24 +22,33 @@ namespace LIEC_Website.Controllers
             "Arrêt sur images", "RTL", "Le Figaro", "Libération", "Le Monde", "La Tribune", "Dauphiné Libéré", "Le Parisien"
         };
 
-        private static string[] _imageSources = new[]
-        {
-            "https://pbs.twimg.com/media/DdQaOTfWsAIv3yK.png"
-        };
+        private static List<string> _imageSources = new List<string>();
+
+        public InfoDataController(IHostingEnvironment hostingEnvironment){
+            _hostingEnvironment = hostingEnvironment;
+
+            // Get all visual paths
+            var rootpath = $"{hostingEnvironment.WebRootPath}\\static\\";
+            DirectoryInfo d = new DirectoryInfo(rootpath);
+            FileInfo[] Files = d.GetFiles("*.png"); 
+            foreach (FileInfo file in Files)
+            {
+                _imageSources.Add($"static\\{file.Name}");
+            }
+        }
 
         [HttpGet("[action]")]
         public IEnumerable<InfoLiecData> InfoLiec()
         {
-            var rng = new Random();
-            var test  =  Enumerable.Range(1, 80).Select(index => new InfoLiecData
+            var info  =  Enumerable.Range(1, 80).Select(index => new InfoLiecData
             { 
                 Title = "Le Bonus Cultura, un modèle ?",
                 Context = "Le pass culture, co-financé par les #GAFAM, est-il une bonne solution pour promouvoir la culture chez les jeunes ?",
-                Image = _imageSources.First(),
-                Sources  = _sources.OrderBy(x => x.IndexOf(x) % 2 == (int)(new Random(2).NextDouble()%2)).Take(2).ToList().ToArray(),
-                Tags = _tags.OrderBy(x => x.IndexOf(x) % 2 == (int)(new Random(2).NextDouble()%2)).Take(3).ToList().ToArray(),
+                Image = _imageSources.OrderBy(x => Guid.NewGuid()).Take(1).ToList().First(),
+                Sources  = _sources.OrderBy(x => Guid.NewGuid()).Take(2).ToList().ToArray(),
+                Tags = _tags.OrderBy(x => Guid.NewGuid()).Take(3).ToList().ToArray(),
             });
-            return test;
+            return info;
         }
 
         public class InfoLiecData
