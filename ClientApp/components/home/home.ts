36ab676b40275eb17397a0399594a4618ef30ef3
Alexius Calamus <AlexiusCalamus@users.noreply.github.com>
2018-06-06 00:44:45 +0200

    Fade out of top bar and fade in of scroll to top overlay  when scrolling

--- ClientApp/components/home/home.ts
+++ ClientApp/components/home/home.ts
@@ -1,34 +1,58 @@
-import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import $ from "jquery";
 
 interface InfoLiec {
-    title: string;
-    context: string;
-    image: string;
-    sources: string[];
-    tags: string[];
-    theme: string;
+  title: string;
+  context: string;
+  image: string;
+  sources: string[];
+  tags: string[];
+  theme: string;
 }
 
 @Component
 export default class HomeComponent extends Vue {
-    infos: InfoLiec[] = [];
+  infos: InfoLiec[] = [];
 
-    mounted() {
-        fetch('api/InfoData/InfoLiec')
-            .then(response => response.json() as Promise<InfoLiec[]>)
-            .then(data => {
-                this.infos = data;
-            });
-    }
+  destroy() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
 
-    openModal(){
-        var modal = document.getElementById("modalwindow") || null;
-        modal!.style.display = "block";
+  mounted() {
+    fetch("api/InfoData/InfoLiec")
+      .then(response => response.json() as Promise<InfoLiec[]>)
+      .then(data => {
+        this.infos = data;
+      });
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll() {
+    console.log("scrolling...");
+    $(window).scroll(function() {
+      if ($(window)!.scrollTop() || 0 > 0) {
+        $(".top-nav").fadeOut();
+        $(".scrollToTop").fadeIn();
+      } else {
+        $(".top-nav").fadeIn();
+        $(".scrollToTop").fadeOut();
       }
+    });
+  }
 
-    closeModal(){
-          var modal = document.getElementById("modalwindow");
-        modal!.style.display = "none";
-      }
+  scrollToTop(){
+    $(".scrollToTop").fadeOut();
+    window.scrollTo(0, 0);    
+  }
+
+  openModal() {
+    var modal = document.getElementById("modalwindow");
+    modal!.style.display = "block";
+  }
+
+  closeModal() {
+    var modal = document.getElementById("modalwindow");
+    modal!.style.display = "none";
+  }
 }
