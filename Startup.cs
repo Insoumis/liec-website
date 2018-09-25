9fbd8764a73065dfee17f358fa5804f9d3f81860
Alexius Calamus <AlexiusCalamus@users.noreply.github.com>
2018-09-25 11:48:37 +0200

    Begin External Authentification

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,12 @@
 using LIEC_Website.Data;
 using LIEC_Website.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
@@ -23,12 +28,41 @@ namespace LIEC_Website
         {
             services.AddMvc();
 
+            // Add MongoDB authentification service
             services.AddIdentityWithMongoStores(Configuration.GetConnectionString("MongoDB") + "/users")
                 .AddDefaultTokenProviders();
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
 
+            // Add external authentification (google, facebook...)
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(o => o.LoginPath = new PathString("/login"))
+                .AddFacebook(o =>
+                {
+                    o.AppId = Configuration["facebook:appid"];
+                    o.AppSecret = Configuration["facebook:appsecret"];
+                })
+                .AddGoogle(o =>
+                {
+                    o.ClientId = "YOUR_CLIENT_ID";
+                    o.ClientSecret = "YOUR_CLIENT_SECRET";
+                    //o.CallbackPath = new PathString("/signin-google");
+                    //o.SignInScheme = "ExternalCookie";
+                })
+                .AddTwitter(o =>
+                {
+                    o.ConsumerKey = "YOUR_CLIENT_ID";
+                    o.ConsumerSecret = "YOUR_CLIENT_SECRET";
+                    //o.CallbackPath = new PathString("/signin-google");
+                    //o.SignInScheme = "ExternalCookie";
+                });
+
+            // Use OpenId to authenticate with Discord
+            // https://kevinchalet.com/2016/07/13/creating-your-own-openid-connect-server-with-asos-introduction/
+            // https://discordapp.com/developers/docs/topics/oauth2
+
+            // Configure MongoDB
             MongoDbContext.Configure(Configuration.GetConnectionString("MongoDB"));
         }
 
